4f66143b56115b350cac91a9625f1aece82e8868
Bradley McElroy <bradley.mcelroy@live.com>
2020-04-04 02:43:31 +0000

    Feature/cache validators

--- deche/core.py
+++ deche/core.py
@@ -1,40 +1,26 @@
 import datetime
 import functools
 import hashlib
-import inspect
 import pathlib
-import time
 from dataclasses import dataclass
 from enum import Enum
-from typing import Callable, Union
+from functools import partial
+from typing import Callable, Union, Tuple, Optional
 
-import fsspec
 from cloudpickle import cloudpickle
 from fsspec import AbstractFileSystem, filesystem
+from fsspec.implementations.local import LocalFileSystem
 
 from deche import config
 from deche.inspection import args_kwargs_to_kwargs
+from deche.util import is_input_filename
+from deche.validators import exists, has_passed_cache_ttl
 
 
 def tokenize(obj: object, serializer: Callable = cloudpickle.dumps) -> (str, bytes):
     value = serializer(obj)
     key = hashlib.sha256(value).hexdigest()
     return key, value
-
-
-def hash_clean_source(func, length=7):
-    src = inspect.getsource(func).split('\n')
-    lines = [l.strip() for l in src if not l.startswith('@')]
-    clean_src = '\n'.join(lines)
-    return hashlib.sha256(clean_src)[:length]
-
-
-def func_qualname(func):
-    if func.__module__ == '__main__':
-        # TODO add tests
-        return f'{func.__module__}/{func.__name__}-{hash_clean_source(func)}'
-    else:
-        return f'{func.__module__}/{func.__name__}'
 
 
 def tokenize_func(func):
@@ -46,13 +32,12 @@ def tokenize_func(func):
     return inner
 
 
+DEFAULT_VALIDATORS = (exists,)
+
+
 class CacheExpiryMode(Enum):
     REMOVE = 1
     APPEND = 2
-
-
-def is_input_filename(key):
-    return key.endswith('.inputs')
 
 
 @dataclass
@@ -63,38 +48,27 @@ class _Cache:
     input_deserializer: Callable = cloudpickle.loads
     output_serializer: Callable = cloudpickle.dumps
     output_deserializer: Callable = cloudpickle.loads
-    cache_ttl: Union[datetime.timedelta, int] = None
+    cache_ttl: Optional[Union[datetime.timedelta, datetime.datetime, int]] = None
     cache_expiry_mode: CacheExpiryMode = CacheExpiryMode.REMOVE
+    cache_validators: Tuple[Callable] = None
 
     def __post_init__(self):
         if self.fs is None:
             self.fs = filesystem(protocol=config.get('fs.protocol'), **config.get("fs.storage_options", {}))
+        if self.cache_validators is None:
+            self.cache_validators = DEFAULT_VALIDATORS
         if isinstance(self.cache_ttl, datetime.timedelta):
             self.cache_ttl = self.cache_ttl.total_seconds()
-
-    def _has_passed_cache_ttl(self, path):
-        modified = self.last_modified(path=path)
-        age = time.time() - modified
-        return age > self.cache_ttl
-
-    def last_modified(self, path):
-        """ Return last modified time as unix timestamps """
-        info = self.fs.info(path=path)
-        if isinstance(self.fs, fsspec.get_filesystem_class('file')):
-            return info['created']
-        elif isinstance(self.fs, fsspec.get_filesystem_class('s3')):
-            return info['LastModified'].timestamp()
-        else:
-            raise NotImplementedError
+        if self.cache_ttl is not None:
+            self.cache_validators += (partial(has_passed_cache_ttl, cache_ttl=self.cache_ttl),)
+        if isinstance(self.fs, LocalFileSystem):
+            assert self.fs.auto_mkdir, "Set auto_mkdir=True when using LocalFileSystem so directories can be created"
 
     def valid(self, path):
-        exists = self.fs.exists(path)
-        if not exists:
-            return False
-        elif not self.cache_ttl:
-            return exists
-        else:
-            return not self._has_passed_cache_ttl(path=path)
+        return all((validator(fs=self.fs, path=path) for validator in self.cache_validators))
+
+    def is_exception(self, path):
+        return self.fs.exists(path=f'{path}.exc')
 
     def read(self, path):
         with self.fs.open(path, mode='rb') as f:
@@ -161,6 +135,13 @@ class _Cache:
 
         return inner
 
+    def load_cached_exception(self, func, path, deserializer=None):
+        def inner(*args, **kwargs):
+            key = func.tokenize(*args, **kwargs)
+            return self.read_output(path=f'{path}/{key}.exc', deserializer=deserializer)
+
+        return inner
+
     def __call__(self, func):
         path = f"{self.prefix}/{func.__module__}.{func.__name__}"
 
@@ -169,15 +150,22 @@ class _Cache:
             inputs = args_kwargs_to_kwargs(func=func, args=args, kwargs=kwargs)
             key, _ = tokenize(obj=inputs)
             if self.valid(path=f'{path}/{key}'):
-                return self.read_output(path=f'{path}/{key}')
-            output = func(*args, **kwargs)
+                return self.load_cached_data(func=func, path=f'{path}/{key}')
+            elif self.is_exception(path=f'{path}/{key}'):
+                return self.load_cached_exception(func=func, path=path)
+            try:
+                output = func(*args, **kwargs)
+                self.write_output(path=f'{path}/{key}', output=output)
+            except Exception as e:
+                output = e
+                self.write_output(path=f'{path}/{key}.exc', output=output)
             self.write_input(path=f'{path}/{key}', inputs=inputs)
-            self.write_output(path=f'{path}/{key}', output=output)
             return output
 
         inner.tokenize = tokenize_func(func=func)
         inner.is_cached = self.is_cached(path=path, func=inner)
         inner.load_cached_data = self.load_cached_data(path=path, func=inner)
+        inner.load_cached_exception = self.load_cached_exception(path=path, func=inner)
         inner.list_cached_parameters = self.list_cached_parameters(path=path)
         inner.path = path
         return inner
--- setup.py
+++ setup.py
@@ -41,19 +41,55 @@ requirements_file = open(requirements_filename)
 # format when specifying requirements.
 #  - https://pythonhosted.org/setuptools/setuptools.html#declaring-dependencies
 # Furthermore, you can't use line continuations with the following:
-requirements = requirements_file.read().strip().split('\n')
+requirements = requirements_file.read().strip().split("\n")
+
+
+def install_deps():
+    """Reads requirements.txt and preprocess it
+    to be feed into setuptools.
+
+    This is the only possible way (we found)
+    how requirements.txt can be reused in setup.py
+    using dependencies from private github repositories.
+
+    Links must be appendend by `-{StringWithAtLeastOneNumber}`
+    or something like that, so e.g. `-9231` works as well as
+    `1.1.0`. This is ignored by the setuptools, but has to be there.
+
+    Warnings:
+        to make pip respect the links, you have to use
+        `--process-dependency-links` switch. So e.g.:
+        `pip install --process-dependency-links {git-url}`
+
+    Returns:
+         list of packages and dependency links.
+    """
+    default = open("requirements.txt", "r").readlines()
+    new_pkgs = []
+    links = []
+    for resource in default:
+        if "git+ssh" in resource or "git+https" in resource:
+            pkg = resource.split("#")[-1]
+            links.append(resource.strip() + "-9876543210")
+            new_pkgs.append(pkg.replace("egg=", "").rstrip())
+        else:
+            new_pkgs.append(resource.strip())
+    return new_pkgs, links
 
 extras_require = {
     's3': ['s3fs'],
 }
-
 extras_require['complete'] = sum(extras_require.values(), list())
 
+
+pkgs, new_links = install_deps()
+
 setup_args = {
-    'description': parser.get('package', 'description'),
-    'include_package_data': True,
-    'install_requires': requirements,
-    'extras_require': extras_require,
+    "description": parser.get("package", "description"),
+    "include_package_data": True,
+    "install_requires": pkgs,
+    "dependency_links": new_links,
+    "extras_require": extras_require,
     'name': parser.get('package', 'name'),
     'version': parser.get('package', 'version'),
     'url': get_default(parser, 'package', 'url', None),
