b151a1359473e720e64bb29a7063b3a1b85217ac
limx0 <bradley.mcelroy@live.com>
2020-04-17 07:19:50 +1000

    Add lazy instantiation, add pre-commit hooks, black codebase

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,29 +1,29 @@
 import pytest
 
 from deche.core import cache
-from deche.test_utils import mem_fs, path as cache_path, tmp_fs, exc_func
+from deche.test_utils import memory_cache, mem_fs, path as cache_path, exc_func
 from deche.types import FrozenDict
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def cleanup(c: cache, path):
-    files = list(c.fs.glob(f'{path}/**/*'))
+    files = list(c.fs.glob(f"{path}/**/*"))
     for f in files:
         if c.fs.exists(f):
             c.fs.rm(path=f)
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def test_cleanup():
     yield
-    for f in mem_fs.glob(path=f'/**/*'):
+    for f in mem_fs.glob(path=f"/**/*"):
         if mem_fs.exists(f):
             mem_fs.rm(f)
 
 
 @pytest.fixture()
 def c():
-    return cache(fs=mem_fs)
+    return memory_cache
 
 
 @pytest.fixture()
@@ -31,22 +31,22 @@ def path():
     return cache_path
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def inputs():
-    return FrozenDict({'a': '1', 'b': 2, 'c': b'3'})
+    return FrozenDict({"a": "1", "b": 2, "c": b"3"})
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def inputs_key():
-    return '100795f3d6d26a8f8f808cb5589412f5d5f67e8fd8188a42b18f935c04348940'
+    return "100795f3d6d26a8f8f808cb5589412f5d5f67e8fd8188a42b18f935c04348940"
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def output():
-    return b'some data'
+    return b"some data"
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def cached_exception():
     try:
         exc_func()
